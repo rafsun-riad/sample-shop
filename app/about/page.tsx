b2dc379f1e62c2f6808b8730e795067b195613bb
rafsun-riad <mruhaquer@gmail.com>
2025-01-09 01:37:33 +0600

    about and contact

---
+++ app/about/page.tsx
@@ -0,0 +1,66 @@
+function AboutPage() {
+  return (
+    <div className="container mx-auto my-10">
+      <h1 className="text-center text-3xl font-bold mb-3">About Us</h1>
+      <div className="max-w-5xl h-[1px] w-full bg-gray-700 mx-auto mb-5"></div>
+      <p className="text-lg text-gray-600 leading-relaxed text-justify">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
+        inventore pariatur omnis, aliquid animi tempora beatae, veniam
+        perferendis laudantium enim ullam! Vero, tempora fugiat libero similique
+        odit eveniet, reiciendis adipisci vel mollitia praesentium quo?
+        Assumenda perferendis eligendi nihil eveniet velit molestiae explicabo,
+        hic expedita ut odio illo? Harum, possimus! Perspiciatis, porro quisquam
+        dolor debitis magnam quis earum officiis illum id ad accusamus
+        laudantium sunt, eaque esse sit recusandae iusto laborum fugiat facilis.
+        Deleniti, recusandae natus, nisi nemo libero, quasi doloremque
+        reprehenderit harum dolore exercitationem consequuntur eligendi?
+        Possimus, neque optio. Vitae sapiente aliquid qui eum. Cum maiores ea
+        modi totam impedit ab ex obcaecati quibusdam pariatur molestias?
+        Blanditiis voluptatibus recusandae repellendus corporis ullam dolorum
+        nesciunt odit veritatis ad sunt magnam, culpa, facilis, suscipit quidem
+        ipsam aliquam at! Eum, dolorem cum magnam reiciendis quo porro possimus
+        consequatur, rem error id dolores iste unde ratione laudantium provident
+        soluta odio repellat. Quam reiciendis veritatis esse tempore voluptatem
+        nesciunt autem dignissimos repudiandae non porro necessitatibus odio sit
+        aliquam rerum enim modi est commodi quae doloribus ab soluta ea, eaque
+        voluptates. Quam placeat eius maiores minima omnis saepe, voluptatibus
+        sint soluta obcaecati similique autem fugit quis deserunt rem itaque ad.
+        Laboriosam, itaque quod cupiditate sed mollitia temporibus expedita ab,
+        recusandae ad, dolorum aliquam in! Ratione non reiciendis officiis illo
+        eius suscipit, placeat eveniet fuga recusandae excepturi nobis ipsam
+        repudiandae accusantium sunt molestiae laudantium illum dolores. Fugit
+        est ullam asperiores qui inventore. Fugit error nihil dolor aspernatur
+        eligendi! Nihil blanditiis inventore saepe animi sint rerum consequatur,
+        eos possimus autem, excepturi molestias ea odio nesciunt illo provident
+        ipsa sed voluptatibus voluptatum facere aliquid. Rerum voluptate qui,
+        eius porro nulla voluptates architecto, dolorem quasi cumque minus illum
+        inventore libero repellat hic repellendus animi, quia dicta aperiam
+        ullam aliquam eum a illo? Quia voluptatibus totam nemo numquam animi
+        nulla, repudiandae in rerum voluptas maxime. Impedit quos eos ducimus!
+        Corporis deleniti ex labore ullam tempora quaerat cumque exercitationem
+        fugiat dicta neque possimus vero expedita, natus et sint atque quis
+        quia, animi dolore. Error rem earum eos maxime harum sint accusamus
+        perspiciatis, perferendis libero voluptate voluptatum incidunt pariatur
+        et saepe esse, atque possimus, repellat iure explicabo ipsa distinctio
+        cumque nam fugit? Alias amet repudiandae delectus dolor ea, rerum
+        perspiciatis aut aspernatur architecto molestias illum facilis
+        consectetur voluptatem quibusdam sunt asperiores beatae nam. Aspernatur
+        laboriosam id eligendi saepe delectus, quas fuga neque consequatur
+        perspiciatis, nostrum quae aut mollitia maxime voluptatem amet in
+        dolorem facere. At explicabo voluptate quibusdam qui corporis placeat
+        deleniti quo reprehenderit cum debitis fugiat quaerat quae, excepturi
+        asperiores soluta, animi obcaecati hic ipsam error tempora, aut eveniet.
+        Incidunt asperiores fuga vitae debitis numquam et earum unde ratione.
+        Assumenda quisquam expedita doloremque molestiae et animi illum
+        architecto fuga explicabo at. Maiores corporis dolor cum mollitia eum
+        velit ad voluptate inventore, quas officiis repellat nam esse odit
+        repudiandae iste ducimus facere eos quo veniam possimus excepturi
+        ratione consectetur at. Nulla corrupti magni aliquam nemo vel maiores
+        consectetur omnis veniam mollitia, dolorem aspernatur laborum numquam
+        ratione odio doloremque similique repudiandae quos quas voluptas!
+      </p>
+    </div>
+  );
+}
+
+export default AboutPage;
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,32 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
+
 function ContactPage() {
-  return <div></div>;
+  return (
+    <div className="container mx-auto my-10">
+      <h1 className="text-center text-3xl font-bold mb-3">Contact Us</h1>
+      <div className="max-w-5xl h-[1px] w-full bg-gray-700 mx-auto mb-5"></div>
+      <form className="space-y-6 mx-auto w-full max-w-lg">
+        <div className="grid w-full max-w-lg items-center gap-1.5">
+          <Label htmlFor="email">Email</Label>
+          <Input type="email" id="email" placeholder="Enter Your Email" />
+        </div>
+        <div className="grid w-full gap-1.5">
+          <Label htmlFor="message-2">Your Message</Label>
+          <Textarea placeholder="Type your message here." id="message-2" />
+          <p className="text-sm text-muted-foreground">
+            Your message will be copied to the support team.
+          </p>
+        </div>
+        <Button className={cn("px-8 bg-lime-600")} type="submit">
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
 }
 
 export default ContactPage;
